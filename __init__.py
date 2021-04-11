a6b4b7b3b612166d294cd754c54e8236802de345
jay.phillz <58239+jaysonjphillips@users.noreply.github.com>
2021-04-10 22:15:55 -0700

    refactored out coinbase auth

--- __init__.py
+++ __init__.py
@@ -1,32 +1,19 @@
-import os
-import hmac
-import hashlib
-import time
 from mycroft import MycroftSkill, intent_handler
+import CoinbaseWalletAuth
 import requests
+
+API_KEY = os.getenv('CB_KEY')
+API_SECRET = os.getenv('CB_SECRET')
 
 class CryptoSkill(MycroftSkill):
     def __init__(self):
         super(CryptoSkill, self).__init__("CryptoSkill")
+        self.request_auth = new CoinbaseWalletAuth(API_KEY, API_SECRET)
 
     @intent_handler('what.is.my.crypto.balance.intent')
     def get_crypto_balance(self):
-        time_request = requests.get('https://api.coinbase.com/v2/time')
-        result = time_request.json()
-        self.log.info(result)
-        apikey = os.getenv('CB_SECRET', '')
-        signed_payload = hmac.new(
-            key=apikey.encode('utf-8'), 
-            msg=f"{str(result['data']['epoch'])}GET/v2/accounts".encode('utf-8'), 
-            digestmod=hashlib.sha256
-            ).hexdigest()
-
-        headers = {
-            'CB-ACCESS-KEY': os.getenv('CB_KEY'),
-            'CB-ACCESS-SIGN': signed_payload,
-            'CB-ACCESS-TIMESTAMP': f"{str(result['data']['epoch'])}"
-        }
-        r = requests.get('https://api.coinbase.com/v2/accounts', headers=headers)
+        
+        r = requests.get('https://api.coinbase.com/v2/accounts', auth=auth)
         self.log.warn(r.json())
         
         # use some sort of imported service to make request to coinbase API
