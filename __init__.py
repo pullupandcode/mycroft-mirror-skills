8358c589fe5a1ceb20f1eab0fd6ceae46fbe539e
Jay Phillz <58239+jaysonjphillips@users.noreply.github.com>
2021-04-10 18:50:45 -0700

    (fix): timestamp issue  again?

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ class CryptoSkill(MycroftSkill):
 
     @intent_handler('what.is.my.crypto.balance.intent')
     def get_crypto_balance(self):
-        req_time = time.mktime(time.gmtime())
+        req_time = str(time.mktime(time.gmtime()))
         headers = {
             'CB-ACCESS-KEY': os.getenv('CB_KEY'),
             'CB-ACCESS-SIGN': hmac(os.getenv('CB_SECRET'), '%s%s%s'.replace(req_time, 'GET', '/accounts')),
