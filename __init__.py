3b68429420d5015825f106c63b8c8d7e8357cd81
jay.phillz <58239+jaysonjphillips@users.noreply.github.com>
2021-04-10 22:31:26 -0700

    (fix): single file again, keep it simple git push

--- __init__.py
+++ __init__.py
@@ -1,9 +1,25 @@
 from mycroft import MycroftSkill, intent_handler
-from coinbase import CoinbaseWalletAuth
 import requests
 
 API_KEY = os.getenv('CB_KEY')
 API_SECRET = os.getenv('CB_SECRET')
+
+class CoinbaseWalletAuth(AuthBase):
+    def __init__(self, api_key, secret_key):
+        self.api_key = api_key
+        self.secret_key = secret_key
+
+    def __call__(self, request):
+        timestamp = str(int(time.time()))
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        signature = hmac.new(self.secret_key, message, hashlib.sha256).hexdigest()
+
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+        })
+        return request
 
 class CryptoSkill(MycroftSkill):
     def __init__(self):
--- coinbase.py
+++ coinbase.py
@@ -4,19 +4,3 @@ from requests.auth import AuthBase
 # Before implementation, set environmental variables with the names API_KEY and API_SECRET
 
 # Create custom authentication for Coinbase API
-class CoinbaseWalletAuth(AuthBase):
-    def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
-
-    def __call__(self, request):
-        timestamp = str(int(time.time()))
-        message = timestamp + request.method + request.path_url + (request.body or '')
-        signature = hmac.new(self.secret_key, message, hashlib.sha256).hexdigest()
-
-        request.headers.update({
-            'CB-ACCESS-SIGN': signature,
-            'CB-ACCESS-TIMESTAMP': timestamp,
-            'CB-ACCESS-KEY': self.api_key,
-        })
-        return request
