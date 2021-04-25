b850e44af3d265c7b1995dd7d8cd7c7d2a89d553
Jay Phillz <58239+jaysonjphillips@users.noreply.github.com>
2021-04-24 19:16:30 -0700

    (fix): you're gonna hate yourself later for this msg

--- __init__.py
+++ __init__.py
@@ -41,7 +41,7 @@ class CryptoSkill(MycroftSkill):
 
         response_message = self.parse_cb_response(result.get("data"))
 
-        self.redis_client.publish('crypto_balance', json.dumps(response_message), separators=(',', ':'))
+        self.redis_client.publish('crypto_balance', json.dumps(response_message, separators=(',', ':')))
         self.log.info('==== message published ====')
     
     def parse_cb_response(self, data):
